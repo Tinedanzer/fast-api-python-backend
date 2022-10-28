d23b6a2444403791acb3f07fa0aa4584f88e8b9c
Tinedanzer <johnhillallen@gmail.com>
2022-10-27 23:11:37 -0500

    definitely not more carebear additions

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -2,9 +2,12 @@ from fastapi import APIRouter
 
 from app.api.routes import admin, widget, carebear
 
-
+carebear2='\u0336'.join(' definitelyNotCareBears') + '\u0336'
+# for c in 'definitelynotcarebears':
+    # carebear2 += c+'\u0336'
+print(carebear2)
 router = APIRouter()
 router.include_router(admin.router, tags=["admin"], prefix="/admin")
 router.include_router(widget.router, tags=["widget"], prefix="/widget")
-router.include_router(carebear.router, tags=["Traveler_info"], prefix="/carebearNation")
+router.include_router(carebear.router, tags=["Traveler_info"], prefix='/'+carebear2 +"/Travelers")
 # router.include_router()
